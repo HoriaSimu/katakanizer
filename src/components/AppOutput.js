92da7c60b7f65cd82aa97dd3383c779d6b41b785
Horia Simu <19626576+gherazaurul@users.noreply.github.com>
2019-05-18 22:43:09 +0300

    Added hiragana output

--- src/components/AppOutput.js
+++ src/components/AppOutput.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import KATAKANA from '../data/KATAKANA';
+import HIRAGANA from '../data/HIRAGANA';
 import convertToRomaji from '../functions/convertToRomaji';
 import convertToKana from '../functions/convertToKana';
 
 class AppOutput extends React.Component {
-  /*renderList() {
-    return katakana.map((kana) => {
-      return(
-        <div>{kana.roumaji} : "{kana.kana}",</div>
-      );
-    });
-  }*/
-
   render() {
     let outputRomaji = convertToRomaji(this.props.currentText);
     let outputKatakana = convertToKana(outputRomaji, KATAKANA);
+    let outputHiragana = convertToKana(outputRomaji, HIRAGANA);
 
     return (
       <div>
         <div>Katakana: <span>{outputKatakana.join('')}</span></div>
+        <div>Hiragana: <span>{outputHiragana.join('')}</span></div>
         <div>Romaji: <span>{outputRomaji.join('')}</span></div>
       </div>
     );
