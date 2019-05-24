b35df2f5c7bd644a70008e8eebb6ce3adc343925
Horia Simu <19626576+gherazaurul@users.noreply.github.com>
2019-05-24 22:54:04 +0300

    Updates

---
+++ precache-manifest.2a8e5881ee9c596a88b0e38f9c7bfda5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "243c2a0a676c8409d7defe309c72ce81",
+    "url": "/katakanizer/index.html"
+  },
+  {
+    "revision": "bcba976ecae54d89863a",
+    "url": "/katakanizer/static/css/main.2a8f2d5c.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/katakanizer/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "bcba976ecae54d89863a",
+    "url": "/katakanizer/static/js/main.39d28ccf.chunk.js"
+  },
+  {
+    "revision": "898e6ce3bb0fd1dc70a5",
+    "url": "/katakanizer/static/js/runtime~main.76b53e15.js"
+  }
+]);
